6b905e6623718059b71a8593e38b2850c2d88676
DARPZZ <rasmustofthermansen@gmail.com>
2024-04-09 17:45:18 +0200

    Clean up unsued parts

--- Rador.py
+++ Rador.py
@@ -8,11 +8,9 @@ GPIO.setmode(GPIO.BCM)
 TRIG = 4
 ECHO = 17
 motor_pin=26
-servo_pin=5
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
-GPIO.setup(servo_pin,GPIO.OUT)
-pwm_servo=GPIO.PWM(servo_pin,50)
+GPIO.setup(motor_pin,GPIO.OUT)
 
 def connect():
     global ws
@@ -20,8 +18,8 @@ def connect():
     print("Connection established")
 
 def readt_the_distance():
-    hest= True
-    while hest:
+    running= True
+    while running:
         GPIO.output(TRIG, True)
         time.sleep(0.00001)
         GPIO.output(TRIG, False)
@@ -32,13 +30,10 @@ def readt_the_distance():
             pulse_duration = pulse_end - pulse_start
             distance = pulse_duration * 17150
             distance = round(distance, 2)
-        print("Distance:"+str(distance) + "cm")
         ws.send(json.dumps({'type': 'distance', 'value': distance}))
-        hest = False
+        running = False
 
 def turn_motor():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(motor_pin,GPIO.OUT)
     pwm_motor=GPIO.PWM(motor_pin,50)
     pwm_motor.start(2.5)
     min_duty_cycle = 2
@@ -47,10 +42,9 @@ def turn_motor():
         for i in np.arange (min_duty_cycle, max_duty_cycle, 0.2):
             pwm_motor.ChangeDutyCycle(i)
             motor_position = (i - min_duty_cycle) / (max_duty_cycle - min_duty_cycle) * 180
-            print(f"Motor position: {motor_position} degrees")
             ws.send(json.dumps({'type': 'degree', 'value': motor_position}))
-            time.sleep(0.3)
             readt_the_distance()
+            time.sleep(0.3)
 
 def main():
     connect()
